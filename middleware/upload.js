cd48a61f13f679a30a1e470751be127ab0ccd2ec
AndyITG123 <andy@itgeeks.com>
2025-07-21 15:23:26 +0530

    backend done 3

---
+++ middleware/upload.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const multer = require('multer');
+const fs = require('fs');
+
+const uploadDir = path.join(__dirname, '../uploads');
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, uploadDir);
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, uniqueSuffix + '-' + file.originalname);
+  }
+});
+
+const upload = multer({ storage });
+
+module.exports = upload;
--- routes/csv.route.js
+++ routes/csv.route.js
@@ -1,9 +1,8 @@
 const express = require('express');
-const multer = require('multer');
 const { auth } = require ("../middleware/auth.js");
 const { handleCSVUpload } = require('../controller/csv.controller');
 
-const upload = multer({ dest: '/tmp' });
+const upload = require('../middleware/upload');
 const router = express.Router();
 router.post('/upload-csv', upload.single('csv'),auth, handleCSVUpload);
 
