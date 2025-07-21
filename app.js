c4184c36d4e294928345e50bd39930a4e5498890
AndyITG123 <andy@itgeeks.com>
2025-07-21 11:52:56 +0530

    doker file

--- app.js
+++ app.js
@@ -24,26 +24,24 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect(process.env.MONGODB_URI)
+app.listen(process.env.PORT, () => {
+    console.log("Server started on port 3000");
+});
 
-    .then(() => {
-        console.log("Connected to MongoDB");
+// mongoose.connect(process.env.MONGODB_URI)
 
-        app.use("/", LoginRouter);
-        app.use("/webhook", WebhookRouter);
-        app.use("/api",SyncRouter);
-          app.use("/api",PageRouter);
-          app.use("/update",UpdateRouter);
-          app.use('/api', CSVRouter);
-        app.use('/exports', express.static(path.join(__dirname, 'public/exports')));
-        app.use("/", ExportRouter);
+//     .then(() => {
+//         console.log("Connected to MongoDB");
 
-
-
-        app.listen(8080, () => {
-            console.log("Server started on port 3000");
-        });
-    })
-    .catch((err) => {
-        console.error("Failed to connect to MongoDB", err);
-    });
+//         app.use("/", LoginRouter);
+//         app.use("/webhook", WebhookRouter);
+//         app.use("/api",SyncRouter);
+//           app.use("/api",PageRouter);
+//           app.use("/update",UpdateRouter);
+//           app.use('/api', CSVRouter);
+//         app.use('/exports', express.static(path.join(__dirname, 'public/exports')));
+//         app.use("/", ExportRouter);
+//     })
+//     .catch((err) => {
+//         console.error("Failed to connect to MongoDB", err);
+//     });
