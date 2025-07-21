a4ee45316ada5ac635b8d21f34ab47e59753e315
AndyITG123 <andy@itgeeks.com>
2025-07-21 19:55:10 +0530

    backend done 5

--- routes/csv.route.js
+++ routes/csv.route.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const { auth } = require ("../middleware/auth.js");
 const { handleCSVUpload } = require('../controller/csv.controller');
+const multer = require('multer');
 
-const upload = require('../middleware/upload');
+const upload = multer({dest: '/tmp'});
 const router = express.Router();
 router.post('/upload-csv', upload.single('csv'),auth, handleCSVUpload);
 
