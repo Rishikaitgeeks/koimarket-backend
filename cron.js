12d5a0071e0b9f7ab04fe0698579ffda3bc23e28
AndyITG123 <andy@itgeeks.com>
2025-07-29 18:59:02 +0530

    websocket

--- cron.js
+++ cron.js
@@ -23,7 +23,6 @@ const syncCron = cron.schedule("*/10 * * * * *", async () => {
 cron.schedule("* * * * * *", async () => {
   let syncData = await syncStatus.findOne({}, '');
   let syncing = syncData?.syncing;
-  console.log('tes',syncing);
   if (syncing) {
     syncCron.start();
   } else {
