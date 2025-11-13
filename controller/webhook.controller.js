028f8b3b7a63821c7b35cd41b45beefccf90e86c
AndyITG123 <andy@itgeeks.com>
2025-11-13 20:12:36 +0530

    clg data

--- controller/webhook.controller.js
+++ controller/webhook.controller.js
@@ -6,7 +6,7 @@ const { setRetailShopifyInventory } = require("../utils/updateStore");
 
 const Webhook = async (req, res) => {
   try {
-    const order = req.body;
+    const order = req.body.order;
     console.log("object");
     const storeName = req.headers["x-shopify-shop-domain"] || null;
     const orderId = order.name;
